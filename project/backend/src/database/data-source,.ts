6ba47b9cbcd301abb28291e6586e5d511399bd21
Andres Armenta <andresadalo25@gmail.com>
2025-11-06 16:59:47 -0500

    usando las variables de entornos en la conexion de la bd

--- project/backend/src/database/data-source,.ts
+++ project/backend/src/database/data-source,.ts
@@ -1,20 +1,20 @@
 import 'reflect-metadata'
 import { DataSource } from "typeorm";
 import dotenv from 'dotenv'
-import {User} from '../entities'
-import {Task} from '../entities'
+import {User} from '../entities/User'
+import {Task} from '../entities/Task'
 
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: ,
-    port : ,
-    username : ,
-    password : ,
-    database :,
-    synchronize : true,
+    host: process.env.DB_HOST  || "localhost",
+    port : Number(process.env.DB_PORT) || 5432,
+    username : process.env.DB_NAME || 'postgres',
+    password : process.env.DB_PASS || 'postgres',
+    database :process.env.DB_NAME || 'postgres',
+    synchronize : true, // en produccion usamos migraciones
     logging :false,
     entities : [User, Task],
 })
